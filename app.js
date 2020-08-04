d26efea62ef9a39507ec1f991ddebf7915ee3b1d
RahulS2904 <69210448+RahulS2904@users.noreply.github.com>
2020-08-04 22:06:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,16 +3,16 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
-};
+const firebaseConfig = {
+  apiKey: "AIzaSyACXE5b0PBE7GqJZMG0zEtrIBD9BXADDwE",
+  authDomain: "glacierm4-94305.firebaseapp.com",
+  databaseURL: "https://glacierm4-94305.firebaseio.com",
+  projectId: "glacierm4-94305",
+  storageBucket: "glacierm4-94305.appspot.com",
+  messagingSenderId: "835499510255",
+  appId: "1:835499510255:web:f7834e5b684cb00e598e1a",
+  measurementId: "G-F1E912M7FB"
+}:
 
      
  
